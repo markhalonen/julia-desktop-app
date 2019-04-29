f3d7507ede8a4237c9b51b9532f341c3db54668b
Mark Halonen <halonen.mark@gmail.com>
2019-04-28 22:43:07 -0400

    zeroMQ

--- ui/src/app.tsx
+++ ui/src/app.tsx
@@ -1,17 +1,21 @@
 import * as React from 'react';
+var zmq = require('zeromq')
+  , sock = zmq.socket('req');
+sock.on('message', (message: any) => {
+  console.log(message.toString())
+})
 
 export class App extends React.Component<undefined, undefined> {
 
 
   addClicked() {
     // producer.js
-    var zmq = require('zeromq')
-      , sock = zmq.socket('req');
+
 
     sock.connect('tcp://localhost:5555');
 
-    var v = sock.send(JSON.stringify({ x: 2, y: 10 }))
-    console.log(v.read())
+    sock.send(JSON.stringify({ x: 2, y: 10 }))
+
   }
 
   render() {
